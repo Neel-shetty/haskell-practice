5549a2ea54b016b007632afd70d457b86dff9d88
Neel Narayan Shetty <neelnarayanshetty@gmail.com>
2024-09-12 16:01:56 +0530

    2nd

--- app/Problems.hs
+++ app/Problems.hs
@@ -4,5 +4,14 @@ myLast :: [a] -> Maybe a
 myLast [] = Nothing
 myLast (x : xs) = if not (null xs) then myLast xs else Just x
 
+-- main = print (myLast [4, 1, 2, 3, 11])
+
+myButLast :: [a] -> Maybe a
+myButLast [] = Nothing
+myButLast (x : (_ : [])) = Just x
+myButLast (_ : xs) = myButLast xs
+
+-- main = print (myButLast [4, 1, 2, 3, 11])
+
 main :: IO ()
-main = print (myLast [4, 1, 2, 3, 11])
+main = print (myButLast [4, 1, 2, 3, 11])
